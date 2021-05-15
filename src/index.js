9f08abfc816061eed4aca3e3bbf7405d1bc4d0fe
Rodolfo <rodofriaslugo@gmail.com>
2021-05-15 15:43:08 -0500

    fix: port

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 const app = require('./app');
 
-app.listen(3000, console.log('Server on port 3000'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, console.log('Server on port', port));
